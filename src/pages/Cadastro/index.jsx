7d3de18b12920383a693701977d798ad770dca95
hudsonmartins08 <hudsonmartins108@gmail.com>
2025-04-30 11:55:26 -0300

    início

---
+++ src/pages/Cadastro/index.jsx
@@ -0,0 +1,7 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+export default function CadastroPage(){
+    return (
+        <p>AQUI é uma página de cadastro</p>
+    )
+}
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,5 @@
+export default function LoginPage(){
+    return(
+        <p>Aqui é página de LOGIN</p>
+    )
+}
