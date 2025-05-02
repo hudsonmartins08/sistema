048bced076e7fe6ef30be5fb7fd2f3f24a20b2a1
hudsonmartins08 <hudsonmartins108@gmail.com>
2025-05-02 11:51:33 -0300

    pagina-login-com-windown

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,25 @@
+import { Link, Navigate, useNavigate } from "react-router-dom"
+import svg from "/public/image-login.svg"
+
 export default function LoginPage(){
+    const navigate = useNavigate()
     return(
-        <p>Aqui é página de LOGIN</p>
+        <div className="w-full h-screen flex">
+            <div className="w-[40%] h-full flex items-center justify-center">
+                <Link to="/cadastro">Cadastrar</Link>
+                <button
+                    onClick={() => navigate("/cadastro")}
+                >Cadastrar</button>
+                <button
+                    onClick={() => window.location.href= "/cadastro"}
+                >
+                    Cadastrar
+                </button>
+            </div>
+            <div className="w-[60%] h-screen bg-[#6366ee] flex items-center justify-center rounded-l-lg flex-col">
+                <img width={600} src={svg} alt="imagem-animada" />
+                <p className="text-white font-bold w-[400px] text-center text-[25px]">A melhor experiência de login que você já teve na sua vida</p>
+            </div>
+        </div>
     )
 }
